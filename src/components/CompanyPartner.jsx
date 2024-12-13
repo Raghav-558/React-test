e9d0ebd1d447e3805479417621c4283eb40371c7
Raghav-558 <jangraraghav9211@gmail.com>
2024-12-13 16:20:54 +0530

    solve updates

--- src/components/CompanyPartner.jsx
+++ src/components/CompanyPartner.jsx
@@ -5,7 +5,7 @@ const CompanyPartner = () => {
     <div className='bg-dark-gray'>
           <div className='max-w-[1140px] mx-auto px-4'>
               <div className=' pt-[63px] pb-[68px] max-lg:pt-[53.17px] max-lg:pb-[59.62px] max-sm:pt-[42.36px] max-sm:pb-[44.84px]'>
-                  <img src="./assets/images/webp/company-partner-image.webp" alt="company-partner-logo" className='lg:w-[573.96px] mx-auto max-lg:w-[436px] max-lg:h-[105px] max-sm:w-[279.73px] max-sm:h-[65.83px]'/>
+                  <img src="./assets/images/webp/company-partner.webp" alt="company-partner-logo" className='lg:w-[573.96px] mx-auto max-lg:w-[436px] max-lg:h-[105px] max-sm:w-[279.73px] max-sm:h-[65.83px]'/>
             </div>
       </div>
     </div>
